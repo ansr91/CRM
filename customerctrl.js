f42c186953e6d6c44e41512dcd21e0d41c091907
ansr91 <ansr91@gmail.com>
2016-06-20 21:50:54 +0530

    no message

--- customerctrl.js
+++ customerctrl.js
@@ -1,30 +1,40 @@
-myApp.controller('customerctrl',function($scope, data) {
-data.getcustomers().succes(JSON.parse(data));
-$scope.customers = data;
-$scope.newcustomer = {name:"",
-                      sex: "",
-                      email:""
-                    };
-
-$scope.addcustomer = function addcustomer(){
-    var names= $scope.newcustomer.name.split(' ');
-    data.addcustomer({
-      first_name:names[0],
-      last_name:names[1],
-      email:$scope.newcustomer.email
-    })
-    .success(customeraddsuccess).error(customeradderror);
+myApp.controller('customerctrl',function($scope) {
+$scope.customers = [ ];
+$scope.newcustomer = {
+  id: "",
+  name: "",
+  sex: "",
+  email: ""
+};
+$scope.addcustomer = function (){
+  $scope.customers.push($scope.newcustomer);
+  window.localStorage.setItem('cus',JSON.stringify($scope.customers));
+  alert('Customer Added Succesfully');
+  $scope.newcustomer = {
+    id: "" ,
+    name:"",
+    sex: "",
+    email: ""
   };
-function customeraddsuccess(data){
-  $scope.error = null;
-  $scope.customers.push(data);
-  $scope.newcustomer = {name:"",
-                        sex: "",
-                        email:""};
-}
+};
 
-function customeradderror(data){
-  $scope.error= data;
-}
 
-});
+$scope.edit = function(){
+  var obj = window.localStorage.getItem('cus');
+  $scope.customers= JSON.parse(obj);
+  var i = $scope.customers.length;
+  if($scope.customers[i].id==$scope.newcustomer.id){
+    $scope.newcustomer = angular.copy($scope.customers[i]);
+    }
+};
+
+$scope.show = function(){
+  var customers = window.localStorage.getItem('cus');
+  if (customers !== null) {
+    customers = JSON.parse(customers);
+  } else {
+    customers = [];
+  }
+  $scope.customers=customers;
+};
+ });
